7b751ce41e3b2c626f0eba428481c28c34d957da
minseok_geum <rmaalstjr1445@gmail.com>
2022-03-20 10:26:40 +0900

    writeXml(File[] file, String path)로 새로 정의 -html문서를 합해서 정리한 문서를 작성하고 xml파일로 저장 processXml(String path)추가 -xml파일의 바디 부분의 내용을 형태소 분석을 하여 그 결과를 내용으로 작성하고 다시 xml파일로 저장

--- src/HtmlParser.java
+++ src/HtmlParser.java
@@ -1,6 +1,11 @@
 import org.jsoup.Jsoup;
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -8,20 +13,21 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
+
 public class HtmlParser {
+
     int count = 0;
     File result[] = new File[1];
 
-    public HtmlParser() throws ParserConfigurationException {
-    }
-
-    // 파일을 찾는 함수
     public File[] findHtml(String dirPath) {//html확장자를 가진 파일들을 찾는 함수
         File dir = new File(dirPath);
         File files[] = dir.listFiles();//모든 파일들의 목록을 배열에 저장
@@ -42,12 +48,14 @@ public class HtmlParser {
         return result;//.html로 끝나는 파일들의 배열을 반환한다.
     }
 
-    //문서 생성
-    DocumentBuilderFactory docFac = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFac.newDocumentBuilder();
-    Document document = docBuilder.newDocument(); // Document 생성
+    //html 파일 리스트와 xml 파일을 생성할 경로를 입력받으면 하나의 통합된 xml파일을 해당 경로에 생성
+    public void writeXml(File[] file, String path) throws IOException, ParserConfigurationException, TransformerException {
+        //문서 생성
+        DocumentBuilderFactory docFac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFac.newDocumentBuilder();
+        Document document = docBuilder.newDocument(); // Document 생성
 
-    public void writeDocs(File[] file) throws IOException {//문서 내용 작성
+        //문서 내용 작성
         Element docs = document.createElement("docs");// element 객체 docs 생성
         document.appendChild(docs);//  document 아래에 docs를 붙인다.
 
@@ -68,11 +76,9 @@ public class HtmlParser {
             body.appendChild(document.createTextNode(bodyData));
             doc.appendChild(body);//doc 하위에 붙인다.
         }
-    }
 
-    public void writeXml(String path_data) throws TransformerException, FileNotFoundException {//xml 파일로 쓰기
+        //xml 파일로 쓰기
         TransformerFactory trfFactory = TransformerFactory.newInstance();
-
         Transformer trformer = trfFactory.newTransformer();
 
         trformer.setOutputProperty(OutputKeys.ENCODING,"UTF-8");
@@ -80,8 +86,40 @@ public class HtmlParser {
         trformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
         DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(new FileOutputStream(new File(path_data+"/collention.xml")));
+        StreamResult result = new StreamResult(new FileOutputStream(new File(path)));
 
         trformer.transform(source, result);
     }
+
+    //xml파일의 바디 태그의 내용을 형태소만 남기고 각 형태소의 등장횟수들로 내용을 수정한다.
+    //가공할 xml파일의 경로를 입력받으면 해당 xml파일을 가공하여 같은 경로에 다시 저장한다.
+    public void processXml(String path) throws TransformerException, ParserConfigurationException, XPathExpressionException, IOException, SAXException {
+        //문서생성
+        DocumentBuilderFactory docFac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFac.newDocumentBuilder();
+        Document doc = docBuilder.parse(path);
+
+        NodeList nodes = null;
+        String bodyData = "";
+        KeywordList kl;
+        KeywordExtractor ke = new KeywordExtractor();
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+        for (int i = 0; i < result.length; i++) {//html 파일의 갯수만큼 문서 안에 doc이 생성되었기 때문에 해당 횟수만큼 반복
+            nodes = (NodeList) xpath.evaluate("//docs/doc[@id = '" + i + "']/body", doc, XPathConstants.NODESET);//docs아래의 doc중 id가 i인 body를 노드에 넣는다
+            bodyData = nodes.item(0).getTextContent();//해당 노드의 내용을 bodyData에 넣는다.
+            kl = ke.extractKeyword(bodyData, true);//bodyData 형태소 분석
+            bodyData = "";
+            for (int j = 0; j < kl.size(); j++) {//분석된 형태소를 문자열로 가공
+                Keyword kwrd = kl.get(j);
+                bodyData = bodyData.concat(kwrd.getString() + ":" + kwrd.getCnt());
+                if(j!=kl.size()-1) bodyData = bodyData.concat("#");
+            }
+            nodes.item(0).setTextContent(bodyData);//최종 결과를 body의 내용으로 입력
+        }
+        // 수정된 xml파일 저장
+        Transformer xformer = TransformerFactory.newInstance().newTransformer();
+        xformer.transform(new DOMSource(doc), new StreamResult(new File(path)));
+    }
 }
+
--- src/SimpleIR.java
+++ src/SimpleIR.java
@@ -1,16 +1,19 @@
+import org.xml.sax.SAXException;
+
 import java.io.*;
 import javax.xml.parsers.*;
 import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
 
 
 public class SimpleIR {
-    public static void main(String args[]) throws ParserConfigurationException, IOException, TransformerException {
+    public static void main(String args[]) throws ParserConfigurationException, IOException, TransformerException, XPathExpressionException, SAXException {
 
         String path_data = "/Users/minseok_geum/Desktop/오픈소스SW입문1/실습/SimpleIR/data";
         HtmlParser parser = new HtmlParser();
+        File[] file = parser.findHtml(path_data);//file은 html 확장자를 가지는 파일들의 배열
 
-        File[] file = parser.findHtml(path_data);//file은 html확장자를 가지는 파일들의 배열
-        parser.writeDocs(file);
-        parser.writeXml(path_data);
+        parser.writeXml(file, path_data+"/collection.xml");
+        parser.processXml(path_data+"/collection.xml");
     }
 }
