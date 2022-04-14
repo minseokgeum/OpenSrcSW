1d49c90ae1a81661e657030eeeaf45a942468c37
minseok_geum <rmaalstjr1445@gmail.com>
2022-04-14 12:14:53 +0900

    Week 6 Version -unmerged feature -changed method to merge

--- src/Searcher.java
+++ src/Searcher.java
@@ -16,11 +16,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class Searcher {
     private String input_file;
     private String query;
+    private int numberOfDoc;
 
     public Searcher(String file, String query){
         this.input_file = file;
@@ -98,92 +98,10 @@ public class Searcher {
             System.out.println("검색된 문서가 없습니다.");
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked", "nls"}) //경고무시
-    public String[] calcSim() throws IOException, ClassNotFoundException, ParserConfigurationException, SAXException, XPathExpressionException {
-        //id 갯수세기
-        DocumentBuilderFactory docFac = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFac.newDocumentBuilder();
-        Document doc = docBuilder.parse("./index.xml");//id 갯수를 셀 index.xml 파일 열기
-        //Document doc = docBuilder.parse(input_file+"/index.xml");// id 갯수를 셀 index.xml 파일 열기
-
-        XPath xpath = XPathFactory.newInstance().newXPath();
-        NodeList nodes = (NodeList)xpath.evaluate("//docs/doc", doc, XPathConstants.NODESET);
-        int numberOfDoc = nodes.getLength();
-
-        //형태소 분석을 위한 객체 생성
-        KeywordList kl;
-        KeywordExtractor ke = new KeywordExtractor();
-        kl = ke.extractKeyword(query, true);//query 형태소 분석
-        Keyword kwrd;
-
-        //index.post 해쉬맵 열기
-        FileInputStream fileStream = new FileInputStream(input_file);
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
-
-        Object object = objectInputStream.readObject();
-        objectInputStream.close();
-
-        HashMap hashMap = (HashMap)object;
-
-        //calculate similarity
-        String[] keywordOfQuery = new String[kl.size()];//Query의 키워드값들을 저장하는 배열
-        int[] tfOfQuery = new int[kl.size()];//Query의 각 키워드들의 tf값
-        ///////
-        double[] resultSimilarity;
-        double[] arrToSort;
-        ///////
-        resultSimilarity = new double[numberOfDoc*2];//짝수 인덱스에는 id 값이, 짝수 인덱스 바로 다음 인덱스에는 해당 id의 유사도 값이 저장되어있다.
-        arrToSort = new double[numberOfDoc];//정렬용 배열, 인덱스 값이 id 값이며, 배열에 저장된 값은 Query와의 similarity 계산결과이다.
-
-        for(int i = 0 ; i < numberOfDoc ; i++) // id값 저장
-            resultSimilarity[2*i] = i;
-
-        for (int i = 0; i < kl.size(); i++) {//Query의 각각의 키워드들에 대해 id 별 similarity를 계산한다.
-            kwrd = kl.get(i);
-            keywordOfQuery[i] = kwrd.getString();
-            tfOfQuery[i] = kwrd.getCnt();
-            if(hashMap.containsKey(keywordOfQuery[i])){
-                String[] weightListOfQuery = ((String)hashMap.get(keywordOfQuery[i])).split(" ");//weightListOfQuery : 짝수 인덱스에는 id 값이, 짝수 인덱스 바로 다음에는 해당 id의 weight값이 저장되어있다.
-                for(int j = 0 ; j < weightListOfQuery.length ; j+=2){//현재 탐색하고 있는 Query의 키워드가 존재하는 id의 similarity에 해당 id에서의 weight값과 현재 키워드의 query에서의 tf값을 곱해서 더해준다.
-                    resultSimilarity[2*Integer.parseInt(weightListOfQuery[j])+1] += tfOfQuery[i]*Double.parseDouble(weightListOfQuery[j+1]);
-                    arrToSort[Integer.parseInt(weightListOfQuery[j])] += tfOfQuery[i]*Double.parseDouble(weightListOfQuery[j+1]);
-                }
-            }else break;
-        }
-
-        //Test lines
-        /*Iterator<String> it = hashMap.keySet().iterator();
-        while(it.hasNext()){
-            String key = it.next();
-            String value = (String)hashMap.get(key);
-            System.out.println(key+" -> "+value);
-        }
-        System.out.println("");
-        for(int i = 0 ; i < keywordOfQuery.length ; i++){
-            if(hashMap.containsKey(keywordOfQuery[i])){
-                String value = (String)hashMap.get(keywordOfQuery[i]);
-                System.out.println(keywordOfQuery[i] +" -> "+value);
-            }else
-                System.out.println(keywordOfQuery[i] + "-> " );//뽑아낸 키워드가 문서에서 존재하지 않을 경우 value를 출력하지않는다.
-        }
-        System.out.println("");
-
-        for(int i = 0 ; i < numberOfDoc ; i++)
-            System.out.println("id "+(int)resultSimilarity[i*2]+ " : " + String.format("%.2f", resultSimilarity[i*2+1]));
-        System.out.println();*/
-        ///////////////////////
-        mergeSort(arrToSort, resultSimilarity, 0, arrToSort.length-1);//정렬용 결과를 정렬하며, 최종 결과인 resultSimilarity 또한 동시에 내림차순으로 정렬을 한다. 이때 similarity id도 similarity와 함께 동시에 정렬된다.
-
-        String[] resultString = new String[numberOfDoc*2];
-
-        for(int i = 0 ; i < numberOfDoc*2 ; i++){
-            if(i%2==0){
-                int id = (int)resultSimilarity[i];
-                nodes = (NodeList) xpath.evaluate("//docs/doc[@id = '" + id + "']/title", doc, XPathConstants.NODESET);
-                resultString[i] = nodes.item(0).getTextContent();
-            }else
-                resultString[i] = String.format("%.2f", resultSimilarity[i]);
-        }
-        return resultString;
+    private void innerProduct(double[] result, int[] tfOfQuery, String[] weightListOfQuery, int indexI, int indexJ){
+        if (result.length == numberOfDoc)
+            result[Integer.parseInt(weightListOfQuery[indexJ])] += tfOfQuery[indexI]*Double.parseDouble(weightListOfQuery[indexJ+1]);
+        else if(result.length == 2*numberOfDoc)
+            result[2*Integer.parseInt(weightListOfQuery[indexJ])+1] += tfOfQuery[indexI]*Double.parseDouble(weightListOfQuery[indexJ+1]);
     }
 }
